69d6a47024c5b49bcfae6cbbd469e542494c70d1
ᎹᎦᎵ <m.conrad.202@gmail.com>
2022-07-31 21:38:11 -0400

    automated pronounce to syllabary work

--- chrutils.py
+++ chrutils.py
@@ -21,76 +21,78 @@ def char_range(c1, c2):
 translit2syl: dict = dict()
 translit2syl_vowels: list = ["a", "e", "i", "o", "u", "v"]
 
-for syl, vowel in zip(char_range("ꭰ", "ꭵ"), translit2syl_vowels):
+for syl, vowel in zip(char_range("Ꭰ", "Ꭵ"), translit2syl_vowels):
     translit2syl[vowel] = syl
 
-translit2syl["ga"] = "ꭶ"
-translit2syl["ka"] = "ꭷ"
+translit2syl["ga"] = "Ꭶ"
+translit2syl["ka"] = "Ꭷ"
 
-for syl, vowel in zip(char_range("ꭸ", "ꭼ"), translit2syl_vowels[1:]):
+for syl, vowel in zip(char_range("Ꭸ", "Ꭼ"), translit2syl_vowels[1:]):
     translit2syl["g" + vowel] = syl
     translit2syl["k" + vowel] = syl
 
-for syl, vowel in zip(char_range("ꭽ", "ꮂ"), translit2syl_vowels):
+for syl, vowel in zip(char_range("Ꭽ", "Ꮂ"), translit2syl_vowels):
     translit2syl["h" + vowel] = syl
 
-for syl, vowel in zip(char_range("ꮃ", "ꮈ"), translit2syl_vowels):
+for syl, vowel in zip(char_range("Ꮃ", "Ꮈ"), translit2syl_vowels):
     translit2syl["l" + vowel] = syl
 
-for syl, vowel in zip(char_range("ꮉ", "ꮍ"), translit2syl_vowels[:-1]):
+for syl, vowel in zip(char_range("Ꮉ", "Ꮍ"), translit2syl_vowels[:-1]):
     translit2syl["m" + vowel] = syl
 
-translit2syl["na"] = "ꮎ"
-# translit2syl["nah"] = "ꮐ"
-translit2syl["hna"] = "ꮏ"
+translit2syl["na"] = "Ꮎ"
+translit2syl["naH"] = "Ꮐ"
+translit2syl["hna"] = "Ꮏ"
 
-for syl, vowel in zip(char_range("ꮑ", "ꮕ"), translit2syl_vowels[1:]):
+for syl, vowel in zip(char_range("Ꮑ", "Ꮕ"), translit2syl_vowels[1:]):
     translit2syl["n" + vowel] = syl
     translit2syl["hn" + vowel] = syl
 
-for syl, vowel in zip(char_range("ꮖ", "ꮛ"), translit2syl_vowels):
+for syl, vowel in zip(char_range("Ꮖ", "Ꮛ"), translit2syl_vowels):
     translit2syl["gw" + vowel] = syl
     translit2syl["kw" + vowel] = syl
 
-translit2syl["sa"] = "ꮜ"
-translit2syl["s"] = "ꮝ"
+translit2syl["sa"] = "Ꮜ"
+translit2syl["s"] = "Ꮝ"
 
-for syl, vowel in zip(char_range("ꮞ", "ꮢ"), translit2syl_vowels[1:]):
+for syl, vowel in zip(char_range("Ꮞ", "Ꮢ"), translit2syl_vowels[1:]):
     translit2syl["s" + vowel] = syl
 
-translit2syl["da"] = "ꮣ"
-translit2syl["de"] = "ꮥ"
-translit2syl["di"] = "ꮧ"
-translit2syl["do"] = "ꮩ"
-translit2syl["du"] = "ꮪ"
-translit2syl["dv"] = "ꮫ"
+translit2syl["da"] = "Ꮣ"
+translit2syl["de"] = "Ꮥ"
+translit2syl["di"] = "Ꮧ"
+translit2syl["do"] = "Ꮩ"
+translit2syl["du"] = "Ꮪ"
+translit2syl["dv"] = "Ꮫ"
 
-translit2syl["ta"] = "ꮤ"
-translit2syl["te"] = "ꮦ"
-translit2syl["ti"] = "ꮨ"
-translit2syl["to"] = "ꮩ"
-translit2syl["tu"] = "ꮪ"
-translit2syl["tv"] = "ꮫ"
+translit2syl["ta"] = "Ꮤ"
+translit2syl["te"] = "Ꮦ"
+translit2syl["ti"] = "Ꮨ"
+translit2syl["to"] = "Ꮩ"
+translit2syl["tu"] = "Ꮪ"
+translit2syl["tv"] = "Ꮫ"
 
-translit2syl["dla"] = "ꮬ"
-translit2syl["tla"] = "ꮭ"
+translit2syl["dla"] = "Ꮬ"
+translit2syl["tla"] = "Ꮭ"
+translit2syl["hla"] = "Ꮭ"
 
-for syl, vowel in zip(char_range("ꮮ", "ꮲ"), translit2syl_vowels[1:]):
+for syl, vowel in zip(char_range("Ꮮ", "Ꮲ"), translit2syl_vowels[1:]):
     translit2syl["dl" + vowel] = syl
+    translit2syl["tl" + vowel] = syl
+    translit2syl["hl" + vowel] = syl
 
-for syl, vowel in zip(char_range("ꮳ", "ꮸ"), translit2syl_vowels):
+for syl, vowel in zip(char_range("Ꮳ", "Ꮸ"), translit2syl_vowels):
     translit2syl["j" + vowel] = syl
+    translit2syl["z" + vowel] = syl
+    translit2syl["ch" + vowel] = syl
 
-for syl, vowel in zip(char_range("ꮹ", "ꮾ"), translit2syl_vowels):
+for syl, vowel in zip(char_range("Ꮹ", "Ꮾ"), translit2syl_vowels):
     translit2syl["w" + vowel] = syl
     translit2syl["hw" + vowel] = syl
 
-for syl, vowel in zip(char_range("ꮿ", "ᏼ"), translit2syl_vowels):
+for syl, vowel in zip(char_range("Ꮿ", "Ᏼ"), translit2syl_vowels):
     translit2syl["y" + vowel] = syl
     translit2syl["hy" + vowel] = syl
-
-for c in " !?,.:":
-    translit2syl[c] = c
 
 translit2syl["h"] = ""  # hopefully intrusive 'h' only
 
@@ -103,20 +105,20 @@ for key in [*translit2syl.keys()]:
 
 
 translit_lookup: list[str] = [*translit2syl.keys()]
-translit_lookup.sort(key=lambda key: len(key), reverse=true)
+translit_lookup.sort(key=lambda key: len(key), reverse=True)
 
 
 def pronounce2syllabary(text: str) -> str:
     text = text.lower().strip()
-    text = re.sub("(?i)[^a-z\\s.,!?]", "", unicodedata.normalize("nfd", text))
+    text = re.sub("(?i)[^a-z\\s.,!?]", "", unicodedata.normalize("NFD", text))
     tmp_syl = ""
     while text:
-        changed: bool = false
+        changed: bool = False
         for lookup in translit_lookup:
             if text.startswith(lookup):
                 tmp_syl += translit2syl[lookup]
                 text = text[len(lookup):]
-                changed = true
+                changed = True
                 break
         if not changed:
             letter = text[0]
@@ -125,7 +127,7 @@ def pronounce2syllabary(text: str) -> str:
                 tmp_syl += "ꮅ"
             else:
                 tmp_syl += letter
-    return unicodedata.normalize("nfc", tmp_syl)
+    return unicodedata.normalize("NFC", tmp_syl)
 
 
 rrd_fix_lookup:dict [str, str] = dict()
@@ -134,14 +136,14 @@ for vowel in translit2syl_vowels:
 
 
 def fix_rrd_pronunciation(pronunciation: str) -> str:
-    pronunciation = unicodedata.normalize("nfd", pronunciation).lower()
+    pronunciation = unicodedata.normalize("NFD", pronunciation).lower()
     prev_pronunciation: str = pronunciation
     for lookup in rrd_fix_lookup:
         if lookup in pronunciation:
             pronunciation = pronunciation.replace(lookup, rrd_fix_lookup[lookup])
     if "ts" in pronunciation and prev_pronunciation == pronunciation:
         pronunciation = pronunciation.replace("ts", "j")
-    return unicodedata.normalize("nfc", pronunciation).lower()
+    return unicodedata.normalize("NFC", pronunciation).lower()
 
 
 def test():
@@ -185,7 +187,7 @@ def ced2mco(text: str):
 
     tones2mco = [("²³", "\u030C"), ("³²", "\u0302"), ("¹", "\u0300"), ("²", ""), ("³", "\u0301"), ("⁴", "\u030b")]
 
-    text = ud.normalize('nfd', text)
+    text = ud.normalize('NFD', text)
 
     # ensure consistent handling of glottal stop variations
     text = re.sub("[\u02c0\u0241\u0242]", "\u0294", text)
@@ -201,12 +203,12 @@ def ced2mco(text: str):
     for ced2mcotone in tones2mco:
         text = text.replace(ced2mcotone[0], ced2mcotone[1])
 
-    return ud.normalize('nfc', text)
+    return ud.normalize('NFC', text)
 
 
 def ascii_ced2mco(text: str):
     import unicodedata as ud
-    text = ud.normalize('nfd', text)
+    text = ud.normalize('NFD', text)
     text = text.replace(".", "\u0323")
     text = text.replace("1", "¹")
     text = text.replace("2", "²")
@@ -218,7 +220,7 @@ def ascii_ced2mco(text: str):
 
 def rrd2mco(text: str):
     import unicodedata as ud
-    text: str = ud.normalize('nfd', text)
+    text: str = ud.normalize('NFD', text)
     text = re.sub("(?i)([aeiouv]\u0323)([^\\s¹²³⁴])", "\\1²\\2", text)
     text = re.sub("(?i)([aeiouv])([^\\s\u0323¹²³⁴])", "\\1²\\2", text)
     return ced2mco(text)
--- syllabary_fixups.py
+++ syllabary_fixups.py
@@ -6,3 +6,64 @@ conda activate audio-lessons
 exec python "$0" "$@"
 exit $?
 ''"""
+# ID|PSET|ALT_PRONOUNCE|PRONOUN|VERB|GENDER|SYLLABARY|PRONOUNCE|ENGLISH|INTRO NOTE|END NOTE|APP_FILE
+import glob
+import pathlib
+import shutil
+
+import chrutils
+
+
+def main() -> None:
+    backup_dir: pathlib.Path = pathlib.Path("data/backup")
+    backup_dir.mkdir(exist_ok=True)
+    for dataset in glob.glob("data/*.txt"):
+        shutil.copy(dataset, backup_dir)
+        with open(dataset, "r") as r:
+            line = r.readline().strip()
+        if "|" not in line:
+            continue
+        header_fields = line.split("|")
+        if len(header_fields) != 12:
+            continue
+        if "SYLLABARY" not in header_fields:
+            continue
+        ix_alt_pronounce = header_fields.index("ALT_PRONOUNCE")
+        ix_pronounce = header_fields.index("PRONOUNCE")
+        ix_syllabary = header_fields.index("SYLLABARY")
+        output_file: str = dataset+".tmp"
+        with open(dataset, "r") as r, open(output_file, "w") as w:
+            w.write(r.readline())
+            for line in r:
+                sline = line.strip()
+                if sline.startswith("#"):
+                    w.write(line)
+                    continue
+                if not sline:
+                    w.write(line)
+                    continue
+                fields = line.strip().split("|")
+                if len(fields) != 12:
+                    w.write(line)
+                    continue
+                if not fields[ix_pronounce] and not fields[ix_alt_pronounce]:
+                    w.write(line)
+                    continue
+                # Move alt pronunciations into main pronounce field
+                if fields[ix_alt_pronounce]:
+                    if fields[ix_pronounce]:
+                        fields[ix_pronounce] += "; "
+                    fields[ix_pronounce] += fields[ix_alt_pronounce]
+                    fields[ix_alt_pronounce] = ""
+                fields[ix_syllabary] = ""
+                for subfield in fields[ix_pronounce].split(";"):
+                    subfield = subfield.strip()
+                    if fields[ix_syllabary]:
+                        fields[ix_syllabary] = fields[ix_syllabary] + "; "
+                    fields[ix_syllabary] = fields[ix_syllabary] + chrutils.pronounce2syllabary(subfield)
+                w.write("|".join(fields))
+                w.write("\n")
+
+
+if __name__ == '__main__':
+    main()
