f2a96bde7dd3d14db31b910e3215d6d472397a4b
ᎹᎦᎵ <m.conrad.202@gmail.com>
2022-07-31 22:56:45 -0400

    Syllabary Lookup

--- syllabary_fixups.py
+++ syllabary_fixups.py
@@ -7,14 +7,53 @@ exec python "$0" "$@"
 exit $?
 ''"""
 # ID|PSET|ALT_PRONOUNCE|PRONOUN|VERB|GENDER|SYLLABARY|PRONOUNCE|ENGLISH|INTRO NOTE|END NOTE|APP_FILE
+import csv
 import glob
 import pathlib
+import re
 import shutil
 
 import chrutils
 
 
 def main() -> None:
+    data_dir: pathlib.Path = pathlib.Path("syllabary_lookup")
+    lookup_file: pathlib.Path = data_dir.joinpath("lookup.csv")
+    syllabary_lookup: dict[str, str] = dict()
+    if lookup_file.exists():
+        with open(lookup_file, "r", newline='') as r:
+            csv_reader = csv.reader(r)
+            first_line: bool = True
+            for row in csv_reader:
+                if first_line:
+                    first_line=False
+                    continue
+                if len(row) < 3:
+                    continue
+                pronounce = row[1].strip().lower()
+                pronounce = re.sub("(?i)[ˀɁɂ]", "ʔ", pronounce)
+                syllabary = row[2].strip()
+                if not pronounce or not syllabary:
+                    continue
+                syllabary_lookup[pronounce] = syllabary
+                
+    for fixes_file in glob.glob("syllabary_lookup/*_fixes.csv"):
+        with open(fixes_file, "r", newline='') as r:
+            csv_reader = csv.reader(r)
+            first_line: bool = True
+            for row in csv_reader:
+                if first_line:
+                    first_line=False
+                    continue
+                if len(row) < 3:
+                    continue
+                pronounce = row[1].strip().lower()
+                pronounce = re.sub("(?i)[ˀɁɂ]", "ʔ", pronounce)
+                syllabary = row[2].strip()
+                if not pronounce or not syllabary:
+                    continue
+                syllabary_lookup[pronounce] = syllabary
+
     backup_dir: pathlib.Path = pathlib.Path("data/backup")
     backup_dir.mkdir(exist_ok=True)
     for dataset in glob.glob("data/*.txt"):
@@ -55,14 +94,45 @@ def main() -> None:
                         fields[ix_pronounce] += "; "
                     fields[ix_pronounce] += fields[ix_alt_pronounce]
                     fields[ix_alt_pronounce] = ""
+                # Make sure we are using the IPA glottal stop
+                fields[ix_pronounce] = re.sub("(?i)[ˀɁɂ]", "ʔ", fields[ix_pronounce])
                 fields[ix_syllabary] = ""
                 for subfield in fields[ix_pronounce].split(";"):
-                    subfield = subfield.strip()
+                    subfield = subfield.strip().lower()
                     if fields[ix_syllabary]:
                         fields[ix_syllabary] = fields[ix_syllabary] + "; "
-                    fields[ix_syllabary] = fields[ix_syllabary] + chrutils.pronounce2syllabary(subfield)
+                    if subfield in syllabary_lookup:
+                        fields[ix_syllabary] = fields[ix_syllabary] + syllabary_lookup[subfield]
+                    else:
+                        new_syllabary: str = chrutils.pronounce2syllabary(subfield)
+                        fields[ix_syllabary] = fields[ix_syllabary] + new_syllabary
+                        if subfield not in syllabary_lookup:
+                            syllabary_lookup[subfield] = new_syllabary
                 w.write("|".join(fields))
                 w.write("\n")
+        shutil.move(output_file, dataset)
+
+        with open(lookup_file.parent.joinpath("lookup.tmp"), "w", newline='') as w:
+            csv_writer = csv.writer(w)
+            csv_writer.writerow(["NEEDS_FIXING", "PRONOUNCE", "SYLLABARY"])
+            keys: list[str] = [*syllabary_lookup]
+            keys.sort()
+            for key in keys:
+                if not key:
+                    continue
+                _ = "*" if re.search("(?i)[a-z]", syllabary_lookup[key]) else ""
+                csv_writer.writerow([_, key, syllabary_lookup[key]])
+        shutil.move(lookup_file.parent.joinpath("lookup.tmp"), lookup_file)
+        with open(lookup_file.parent.joinpath("lookup-bad.csv"), "w", newline='') as w:
+            csv_writer = csv.writer(w)
+            csv_writer.writerow(["NEEDS_FIXING", "PRONOUNCE", "SYLLABARY"])
+            keys: list[str] = [*syllabary_lookup]
+            keys.sort()
+            for key in keys:
+                if not key:
+                    continue
+                if re.search("(?i)[a-z]", syllabary_lookup[key]):
+                    csv_writer.writerow(["*", key, syllabary_lookup[key]])
 
 
 if __name__ == '__main__':
---
+++ syllabary_lookup/README.md
@@ -0,0 +1,16 @@
+# Syllabary Lookup
+
+A table of pronunciation to Syllabary mappings. `CSV` was chosen to simplify editing.
+
+The file `lookup.csv` is regenerated each run of `syllabary_lookup.py` and should not be edited.
+
+Place your desired edits in `user_fixes.csv` where the `user` in `user_fixes.csv` is your handle. For example: ᎹᎦᎵ's file would be named `magali_fixes.csv`. Please use your own file. Use the same format as the main `lookup.csv` file.
+
+If the first field in a row contains a "*" then the pronunciation to Syllabary is incomplete for that entry and it needs a fix added to your fixes file.
+
+When the `syllabary_lookup.py` program is run, it will overwrite any entries from the main `lookup.csv` file with fixes supplied by the various `*_fixes.csv` files then write out the
+corrected file.
+
+The `lookup.csv` file is periodically committed by ᎹᎦᎵ to reflect any new entries that need fixing.
+
+
